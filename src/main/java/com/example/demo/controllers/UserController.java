0083018dea08b04318262bf7a0cc7793dd693a16
Adrian Sanchez <LetMeThinkInYou@bitbucket.org>
2018-04-10 21:33:27 +0200

    Simple design from Bootstrap...

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -6,16 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.example.demo.models.User;
 import com.example.demo.repositories.UserRepository;
 import com.example.demo.repositories.impl.InsecureUserRepository;
+import com.example.demo.repositories.impl.SecureUserRepository;
 
 @Controller
 public class UserController {
